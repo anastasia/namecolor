1e8b087d3eac75ca9d70eb44e897eb0f61ebac06
pablo <pablo.loyola.h@gmail.com>
2018-07-30 23:03:57 +0900

    readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# name-color
+
+Transform a name or description in to a color
+
+This is the code used in [this article](https://pabloloyola.github.io/2018/04/01/color-language.html) where 
+the paper [Character Sequence Models for Colorful Words](https://aclweb.org/anthology/D16-1202)
+is replicated.
+
+This code was tested on Pytorch 0.3.1
+
+## Data
+
+`python download_data.py`
+
+
+## Acknowledgement
+
+Most of the content on this repository is based on the great Pytorch tutorials by [epochx](https://github.com/epochx/pytorch-nlp-tutorial) 
---
+++ download_data.py
@@ -0,0 +1,27 @@
+import time
+from colourlovers import ColourLovers
+from colormath.color_objects import LabColor, sRGBColor
+from colormath.color_conversions import convert_color
+
+# check http://www.colourlovers.com/api
+# https://github.com/elbaschid/python-colourlovers
+
+def rgb2lab(r,g,b):
+    return list(convert_color( 
+        sRGBColor(r/255,g/255,b/255), LabColor).get_value_tuple())
+
+def get_data():
+    cl = ColourLovers()
+    data = [] 
+    for i in range(1, 1000000,100):
+        time.sleep(1)
+        col = cl.colors(num_results=100, result_offset=i)
+        data+=[ [c.title] + rgb2lab(c.rgb.red, c.rgb.blue,c.rgb.green) for c in col]
+    
+    print("Saving...")
+    with open("/home/pablo/data/colors/corpus.data", "w") as f:
+        for color in data:
+            f.write(",".join(color)+"\n")
+
+if __name__ == "__main__":
+    get_data()
