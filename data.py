22863edd33e9259bf1dbf2fa3a1a5261b14de5e8
anastasia <anastasia.aizman@gmail.com>
2018-11-26 13:35:13 -0500

    fix - allowing for the names of colors to contain commas

--- data.py
+++ data.py
@@ -1,23 +1,28 @@
-import os
 from collections import namedtuple
 
 Name = namedtuple('Name', ['index', 'name', 'color'])
 
+
 def read_data(dataset_path):
-    indices = [] 
-    names = [] 
-    colors  = [] 
+    indices = []
+    names = []
+    colors = []
 
     with open(dataset_path, 'r') as f:
-        for i,l in enumerate(f.readlines()):
-            name, l , a ,b =l.strip().split(",")
-
-            if len(name) > 0 :
-                indices.append(i)
+        for idx, line in enumerate(f.readlines()):
+            values = line.strip().split(",")
+            # since the names in the colourlovers db sometimes have commas
+            # we pop the LAB values, and anything that's left over is the name
+            b_val = values.pop()
+            a_val = values.pop()
+            l_val = values.pop()
+            name = ",".join(values)
+            if len(name) > 0:
+                indices.append(idx)
                 names.append(name)
-                colors.append(( l,a,b) ) 
+                colors.append((float(l_val), float(a_val), float(b_val)))
 
-    names_ = [ Name(index, name, color) 
-            for index, name, color in zip(indices, names, colors) ]
+    names_ = [Name(index, name, color)
+              for index, name, color in zip(indices, names, colors)]
 
     return names_
