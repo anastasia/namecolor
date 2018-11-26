f6126c5e8d1bf58f75c41198d2a52d87cd212a0f
anastasia <anastasia.aizman@gmail.com>
2018-11-26 13:36:17 -0500

    fix - spacing

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 import random
 from torch.autograd import Variable
+
 random.seed(42)
 
 UNK = '<UNK>'
@@ -8,64 +9,66 @@ PAD = '<PAD>'
 BOS = '<BOS>'
 EOS = '<EOS>'
 
+
 class VocabItem:
 
     def __init__(self, string, hash=None):
         self.string = string
         self.count = 0
-        self.hash = hash 
-        
+        self.hash = hash
+
     def __str__(self):
-        return  'VocabItem({})'.format(self.string)
+        return 'VocabItem({})'.format(self.string)
 
     def __repr__(self):
-        return  self.__str__()
+        return self.__str__()
+
 
 def tokenizer(x):
-    return list(x) 
+    return list(x)
+
 
 def token_function(x):
     return x.lower()
 
+
 class Vocab:
     def __init__(self, sequences, tokenizer=tokenizer,
-            token_function = token_function, 
-            min_count=0, add_padding= False, add_bos=False,
-            add_eos=False, unk=None):
+                 token_function=token_function,
+                 min_count=0, add_padding=False, add_bos=False,
+                 add_eos=False, unk=None):
 
-        vocab_items= []
+        vocab_items = []
         vocab_hash = {}
         item_count = 0
 
         self.token_function = token_function
         self.tokenizer = tokenizer
-        self.special_tokens = [] 
-        
+        self.special_tokens = []
+
         self.UNK = None
-        self.PAD = None 
+        self.PAD = None
         self.BOS = None
         self.EOS = None
 
         index2token = []
         token2index = {}
 
-
-        for sequence in sequences :
+        for sequence in sequences:
             for item in tokenizer(sequence.name):
                 real_item = token_function(item)
                 if real_item not in vocab_hash:
                     vocab_hash[real_item] = len(vocab_items)
                     vocab_items.append(VocabItem(real_item))
 
-                vocab_items[vocab_hash[real_item]].count +=1
-                item_count +=1
+                vocab_items[vocab_hash[real_item]].count += 1
+                item_count += 1
 
-
-                #if item_count % 100 == 0:
+                # if item_count % 100 == 0:
                 #    print("Reading item {}".format(item_count))
 
         tmp = []
-        if unk : 
+        if unk:
             self.UNK = VocabItem(unk, hash=0)
             self.UNK.count = vocab_items[vocab_hash[unk]].count
             index2token.append(self.UNK)
@@ -96,9 +99,9 @@ class Vocab:
             self.EOS = VocabItem(EOS)
             tmp.append(self.EOS)
             self.special_tokens.append(self.EOS)
-        
+
         if add_padding:
-            self.PAD = VocabItem(PAD)   
+            self.PAD = VocabItem(PAD)
             tmp.append(self.PAD)
             self.special_tokens.append(self.PAD)
 
@@ -115,10 +118,13 @@ class Vocab:
 
     def __getitem__(self, i):
         return self.index2token[i]
+
     def __len__(self):
         return len(self.index2token)
+
     def __iter__(self):
         return iter(self.index2token)
+
     def __contains__(self, key):
         return key in self.token2index
 
@@ -148,10 +154,10 @@ class Vocab:
 class BatchIterator(object):
 
     def __init__(self,
-            sentences, vocab, batch_size,
-            shuffle=False, cuda=False, ids= None):
+                 sentences, vocab, batch_size,
+                 shuffle=False, cuda=False, ids=None):
 
-        self.vocab= vocab
+        self.vocab = vocab
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.pad_id = self.vocab.PAD.hash
@@ -169,12 +175,11 @@ class BatchIterator(object):
             else:
                 self.id_examples.append(i)
 
-            y_example = [float(i) for  i in sentence.color ]
+            y_example = [float(i) for i in sentence.color]
             self.y_examples.append(torch.FloatTensor([y_example]))
         assert len(self.examples) == len(self.y_examples)
         self.cuda = self.is_cuda = cuda
         self.num_batches = (len(self.examples) + batch_size - 1) // batch_size
-
 
     def __len__(self):
         return self.num_batches
@@ -192,18 +197,17 @@ class BatchIterator(object):
 
             raise IndexError("index is greater "
                              "than the number of batches")
-        
-        id_slice = self.id_examples[index * self.batch_size:(index + 1) * self.batch_size] 
+
+        id_slice = self.id_examples[index * self.batch_size:(index + 1) * self.batch_size]
         x_slice = self.examples[index * self.batch_size:(index + 1) * self.batch_size]
         y_slice = self.y_examples[index * self.batch_size:(index + 1) * self.batch_size]
 
         padded_x_slice, x_slice_lengths = pad_list(x_slice, pad_value=self.pad_id)
 
-        y_slice = torch.stack(y_slice).transpose(0,1)
+        y_slice = torch.stack(y_slice).transpose(0, 1)
 
         padded_x_slice = Variable(padded_x_slice)
         y_slice = Variable(y_slice)
-
 
         if self.cuda:
             padded_x_slice = padded_x_slice.cuda()
@@ -213,7 +217,6 @@ class BatchIterator(object):
 
 
 def pad_list(raw_input_list, dim0_pad=None, dim1_pad=None, align_right=False, pad_value=0):
-
     input_list = [torch.LongTensor(sublist) for sublist in raw_input_list]
 
     if not dim0_pad:
@@ -235,4 +238,3 @@ def pad_list(raw_input_list, dim0_pad=None, dim1_pad=None, align_right=False, pa
 
     out = out.t()
     return out, lengths
-
