1ca977d56c87fe5a077e0c25249bb6db2d498115
anastasia <anastasia.aizman@gmail.com>
2018-11-29 16:47:47 -0500

    update - some spacing, some file names, etc

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 import torch
 import tqdm
 import torch.nn as nn
-import os
 import random
+from datetime import datetime
 from data import read_data
 from utils import Vocab, BatchIterator
 from model import BiLSTM
 
-torch.backends.cudnn.benchmark=True
+torch.backends.cudnn.benchmark = True
 
-train_path = '/home/pablo/data/colors/train.data'
-valid_path = '/home/pablo/data/colors/valid.data'
+train_path = 'corpus.data'
+valid_path = 'valid.data'
 
 epochs = 20
 batch_size = 128
@@ -24,39 +24,43 @@ num_layers = 2
 min_count = 2
 pooling = 'max'
 lr = 0.0001
+
 gradient_clipping = 0.25
 embedding_size = 300
 cuda = True
 
-print("Reading the data")
-train_names = read_data(train_path) 
+
+train_names = read_data(train_path)
 valid_names = read_data(valid_path)
+# add valid data to training data to bias our colors a bit
+
+train_names += (valid_names * 5)
+
 random.shuffle(train_names)
 random.shuffle(valid_names)
 
-print("Obtaining the vocabulary")
-vocab = Vocab(train_names, min_count= min_count, add_padding=True)
 
-embedddings = nn.Embedding(len(vocab.index2token),
-                                   embedding_size,
-                                   padding_idx=vocab.PAD.hash)
+vocab = Vocab(train_names, min_count=min_count, add_padding=True)
 
-model = BiLSTM(embeddings=embedddings,
-        hidden_size=hidden_size,
-        num_labels=num_labels,
-        input_dropout=input_dropout,
-        output_dropout=output_dropout,
-        bidirectional=bidirectional,
-        num_layers=num_layers,
-        pooling=pooling)
+embeddings = nn.Embedding(len(vocab.index2token),
+                           embedding_size,
+                           padding_idx=vocab.PAD.hash)
 
+model = BiLSTM(embeddings=embeddings,
+               hidden_size=hidden_size,
+               num_labels=num_labels,
+               input_dropout=input_dropout,
+               output_dropout=output_dropout,
+               bidirectional=bidirectional,
+               num_layers=num_layers,
+               pooling=pooling)
 
 if cuda:
     model.cuda()
 
 print(model)
 
-train_batches = BatchIterator(train_names,vocab, batch_size, cuda=cuda)
+train_batches = BatchIterator(train_names, vocab, batch_size, cuda=cuda)
 valid_batches = BatchIterator(valid_names, vocab, batch_size, cuda=cuda)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -65,21 +69,20 @@ pbar = tqdm.trange(epochs, desc='Training...')
 for epoch in pbar:
     epoch_total = 0
     epoch_loss = 0
-
+    training_loss = 0
     for i, batch in enumerate(train_batches):
         (id_sice, padded_x_slice, x_slice_lengths, y_slice) = batch
         loss, logits = model.forward(padded_x_slice,
-                                                    x_slice_lengths,
-                                                    y_slice)
+                                     x_slice_lengths,
+                                     y_slice)
         loss.backward()
-        torch.nn.utils.clip_grad_norm(model.parameters(),
-                gradient_clipping)
+        torch.nn.utils.clip_grad_norm(model.parameters(), gradient_clipping)
 
         optimizer.step()
         total = y_slice.size(0)
         epoch_total += total
         epoch_loss += loss.data[0]
-
+        training_loss = epoch_loss
         if i % log_interval == 0 and i > 0:
             pbar.write('Training Loss: {}'.format(epoch_loss / log_interval))
             epoch_total = 0
@@ -90,9 +93,9 @@ for epoch in pbar:
 
     for i, batch in enumerate(valid_batches):
         (id_sice, padded_x_slice, x_slice_lengths, y_slice) = batch
-        loss,  logits = model.forward(padded_x_slice,
-                                                  x_slice_lengths,
-                                                  y_slice)
+        loss, logits = model.forward(padded_x_slice,
+                                     x_slice_lengths,
+                                     y_slice)
 
         total = y_slice.size(0)
         test_epoch_total += total
